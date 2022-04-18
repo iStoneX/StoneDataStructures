8644c5b2e6bd3c853ed21a12a59f1cd71f56f303
Stone Flynn <stoneflynn72@gmail.com>
2022-04-17 20:10:08 -0700

    added merge function

--- include/LinkedList.h
+++ include/LinkedList.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include<cmath>
 #include <string>
 
 template <typename T> 
@@ -43,6 +44,109 @@ class LinkedList{
             currNode->next = temp;
         }
     }
+
+    void reverse(){
+        Node<T>* tNode;
+        Node<T>* pNode = nullptr;
+        
+        while(head!=nullptr){
+            tNode = head->next;
+            head->next = pNode;
+            pNode = head;
+            head = tNode;
+        }
+        head = pNode;
+    }
+
+    static LinkedList merge(LinkedList<T> A,LinkedList<T> B){
+        //merges two sorted lists
+        Node<T>* index1 = A.head;
+        Node<T>* index2 = B.head;
+        LinkedList mergedList;
+        while(index1!=nullptr && index2!=nullptr){
+            if(index1->value == index2->value){
+                mergedList.insertAtTail(index1->value);
+                index1 = index1->next;
+                mergedList.insertAtTail(index2->value);
+                index1 = index2->next;
+            } else if(index1->value > index2->value){
+                mergedList.insertAtTail(index2->value);
+                index2 = index2->next;
+            }else if(index1->value < index2->value){
+                mergedList.insertAtTail(index1->value);
+                index1 = index1->next;
+            }
+        }
+        if(index1==nullptr && index2==nullptr){
+            return mergedList;
+        }
+        if(index1==nullptr && index2!=nullptr){
+            while(index1!=nullptr){
+                mergedList.insertAtTail(index1->value);
+                index1 = index1->next;
+            }
+            return mergedList;
+        }
+        if(index1!=nullptr && index2==nullptr){
+            while(index2!=nullptr){
+                mergedList.insertAtTail(index2->value);
+                index2 = index2->next;
+            }
+            return mergedList;
+        }
+        return mergedList;
+    }
+
+    //doesnt work;
+    void sort(){
+        Node<T>* temp = head;
+        if(head==nullptr) std::cout << "Failed to sort, empty list.\n"; return;
+        if(head->next==nullptr) return;
+
+        //deterrmine size;
+        int tSize = 0;
+        while(head!=nullptr){
+            tSize++;
+            head=head->next;
+        }
+
+        //if its 2, sort elements
+        if(tSize==2){
+            head = temp;
+            if(head->value<=head->next->value){
+            } else{
+                head = head->next;
+                head->next = temp;
+                head->next=nullptr;
+            }
+        }
+
+        //else, find middle
+        head = temp;
+        for(int i=1;i<std::ceil(tSize/2);i++){
+            head=head->next;
+        }
+
+        //Create new split lists
+        LinkedList<T> listA;
+        listA.head = head->next;
+        head->next = nullptr;
+        head = temp;
+        LinkedList<T> listB;
+        listB.head = head;
+
+        //continue sorting as necesarry
+        if(tSize==3){
+            listA.sort();
+        } else if (tSize>3){
+            listA.sort();
+            listB.sort();
+        }
+
+        merge(listA,listB);
+    }
+
+
 };
 
 //Overwrite the << operator
@@ -61,6 +165,7 @@ std::ostream& operator<<(std::ostream& os, const LinkedList<T>& ll)
         os << " -> ";
         os << currNode->value;
     }
-    os << '}';
+    os << '}' << '\n';
     return os;
 }
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,27 @@
 #include "..\include\LinkedList.h"
 
 int main(){
-    LinkedList<char> stonesList;
-    for(char i = 'A'; i<='Z'; i++){
-        stonesList.insertAtTail(i);
-    }
-    std::cout << stonesList;
+    LinkedList<int> testList;
+    LinkedList<int> testList2;
+    testList.insertAtTail(1);
+    testList.insertAtTail(3);
+    testList.insertAtTail(5);
+    testList2.insertAtTail(2);
+    testList2.insertAtTail(4);
+    testList2.insertAtTail(6);
+    std::cout << "ListA: " << testList;
+    std::cout << "ListB: " << testList2;
+    LinkedList<int> testList3 = LinkedList<int>::merge(testList, testList2);
+    std::cout << "merge(): " << testList3;
+    testList3.reverse();
+    std::cout << "reverse(): " << testList3;
+    std::cout << "------------------------------\n";
+    LinkedList<int> testList4;
+    testList4.insertAtTail(5);
+    testList4.insertAtTail(1);
+    testList4.insertAtTail(3);
+    std::cout << "ListC: " << testList4;
+    testList4.sort();
+    std::cout << "sort(): " << testList4;
     
 }
